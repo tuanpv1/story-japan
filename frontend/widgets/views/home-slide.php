2337c16ed3390d98dcd5dec364fa3f88da381232
hungnd1 <hungnd1@vivas.vn>
2017-03-25 13:12:24 +0700

    a

--- frontend/widgets/views/home-slide.php
+++ frontend/widgets/views/home-slide.php
@@ -36,3 +36,46 @@ use common\models\Slide;
         </div>
     </div>
 </div>
+<!-- servives -->
+<div class="container">
+    <div class="service ">
+        <div class="col-xs-6 col-sm-3 service-item">
+            <div class="icon">
+                <img alt="services" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/s1.png" />
+            </div>
+            <div class="info">
+                <a href="#"><h3>Free Shipping</h3></a>
+                <span>On order over $200</span>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-3 service-item">
+            <div class="icon">
+                <img alt="services" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/s2.png" />
+            </div>
+            <div class="info">
+                <a href="#"><h3>30-day return</h3></a>
+                <span>Moneyback guarantee</span>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-3 service-item">
+            <div class="icon">
+                <img alt="services" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/s3.png" />
+            </div>
+
+            <div class="info" >
+                <a href="#"><h3>24/7 support</h3></a>
+                <span>Online consultations</span>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-3 service-item">
+            <div class="icon">
+                <img alt="services" src="<?= Yii::$app->getUrlManager()->getBaseUrl() ?>/data/s4.png" />
+            </div>
+            <div class="info">
+                <a href="#"><h3>SAFE SHOPPING</h3></a>
+                <span>Safe Shopping Guarantee</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end services -->
