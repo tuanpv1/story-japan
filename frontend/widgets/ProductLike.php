8e06cce92090d2f542440cb0205d0f121d3ce586
Tuan Pham <tuantrylook@gmail.com>
2019-01-02 15:50:57 +0700

    untest

--- frontend/widgets/ProductLike.php
+++ frontend/widgets/ProductLike.php
@@ -29,20 +29,22 @@ class ProductLike extends Widget
     public static function getProductLike($id)
     {
         $cat = ContentCategoryAsm::findOne(['content_id' => $id]);
-        $content = Content::find()
-            ->select('content.id,content.display_name,content.type,content.short_description,content.price,content.images,content.price_promotion')
-            ->innerJoin('content_category_asm', 'content_category_asm.content_id = content.id')
-            ->innerJoin('category', 'content_category_asm.category_id = category.id')
-            ->andWhere('content.id <> :id_cc', ['id_cc' => $id])
-            ->andWhere(['category.id' => $cat->category_id])
-            ->andWhere(['content.status' => Content::STATUS_ACTIVE])
-            ->orderBy(['content.created_at' => 'DESC'])
-            ->limit(6)
-            ->all();
-        $t = new ProductLike();
-        return $t->render('product-like', [
-            'content' => $content,
-        ]);
+        if($cat){
+            $content = Content::find()
+                ->select('content.id,content.display_name,content.type,content.short_description,content.price,content.images,content.price_promotion')
+                ->innerJoin('content_category_asm', 'content_category_asm.content_id = content.id')
+                ->innerJoin('category', 'content_category_asm.category_id = category.id')
+                ->andWhere('content.id <> :id_cc', ['id_cc' => $id])
+                ->andWhere(['category.id' => $cat->category_id])
+                ->andWhere(['content.status' => Content::STATUS_ACTIVE])
+                ->orderBy(['content.created_at' => 'DESC'])
+                ->limit(6)
+                ->all();
+            $t = new ProductLike();
+            return $t->render('product-like', [
+                'content' => $content,
+            ]);
+        }
     }
 
     public static function getEpisode($id)
